03e11aec27f11b093cc9cab112af082166e56aba
dixitakothari7 <dixitadkothari7@gmail.com>
2025-03-15 16:04:14 +0530

    Initial code setup

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat.component.html',
+  styleUrl: ''
+})
+export class ChatComponent {
+
+}
